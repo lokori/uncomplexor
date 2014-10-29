0bc91ca23d398bd330acc6ee02828eb3e3212d40
Antti Virtanen <antti.virtanen@iki.fi>
2014-10-29 17:27:05 +0200

    clojars hook

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject org.clojars.lokori/uncomplexor "0.1.0-SNAPSHOT"
+(defproject org.clojars.lokori/uncomplexor "0.1.0"
   :description "Code complexity measurement for Clojure"
   :url "https://github.com/lokori/uncomplexor"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm "https://github.com/lokori/uncomplexor"
+  :signing {:gpg-key "antti.virtanen@iki.fi"}
   :eval-in-leiningen true
   :uncomplexor {:threshold 65
   	        :branch-penalty 30
